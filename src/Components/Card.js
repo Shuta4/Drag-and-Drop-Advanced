4012818439d89d318d1edd5c9d30dae44a5e2af4
SkirdinNikita <skirdin.nik@gmail.com>
2020-02-23 07:52:54 +0300

    Update from 23.02.2020

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -4,10 +4,10 @@ class Card {
         this._element = null;
         this._container = container;
         this._dragging = false;
-        this._content = document.querySelector(".content");
         this.render = this.render.bind(this);
         this._body = document.querySelector("body");
         this.setEventListeners = this.setEventListeners.bind(this);
+        this.move = this.move.bind(this);
     }
     _template() {
         return `<div class="column__item card">
@@ -20,6 +20,11 @@ class Card {
         this._element = arr[arr.length - 1];
         this.setEventListeners();
     }
+    move(container) {
+        this._container = container;
+        this._element.remove();
+        this.render()
+    }
     setEventListeners() {
         this._body.addEventListener('mousemove', (event) => {
             if(this._dragging) {
@@ -27,21 +32,22 @@ class Card {
                     X: event.clientX,
                     Y: event.clientY
                 }
-                console.log(mousePos)
                 var topOffset = this._element.offsetHeight / 2;
                 var leftOffset = this._element.offsetWidth / 2;
                 var left = mousePos.X - leftOffset;
                 var top = mousePos.Y - topOffset;
-                console.log(left + " " + top)
                 this._element.style.top = top + 'px';
                 this._element.style.left = left + 'px';
             }
         })
         this._element.addEventListener('mousedown', () => {
             this._dragging = true;
+            this._element.style.position = "absolute"
         });
         this._body.addEventListener('mouseup', () => {
             this._dragging = false;
+            var newContainer = this._container;
+            this.move(newContainer)
         });
     }
 }
